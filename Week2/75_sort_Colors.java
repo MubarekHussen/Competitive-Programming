4e703f5f6e46644f4ecde841c3c24afca40be07d
MubarekHussen <mubahussen2014@gmail.com>
2022-04-28 01:27:06 +0300

    sort_colors

---
+++ Week2/75_sort_Colors.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int i, j;
+        
+        for (i = 0; i < nums.length; i++)
+        {
+            for (j = 0; j < nums.length - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
