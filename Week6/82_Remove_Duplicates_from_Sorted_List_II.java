4bb81ccd079aca8ad43d288fba7c0f23e6d4da67
MubarekHussen <mubahussen2014@gmail.com>
2022-10-04 01:24:59 +0300

    removing_duplicate

---
+++ Week6/82_Remove_Duplicates_from_Sorted_List_II.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy =new ListNode();
+        dummy.next = head;
+        ListNode cur = dummy;
+        
+        while(head != null) {
+            if(head.next != null && head.val == head.next.val) {
+                while(head != null && head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                cur.next = head.next;
+            }else {
+                cur = cur.next;
+            }
+            head = head.next;
+        }
+        return (dummy.next);
+    }
+}
